3dcd3aa4112a66bd9076890e7f866e6556f1a541
naomiHauret <hauretnaomi@gmail.com>
2025-10-24 14:54:53 +0200

    chore: cleanup repo after migrating code from packages/contracts to packages/engine

--- packages/contracts/src/components/amount.ts
+++
@@ -1,9 +0,0 @@
-/**
- * Amount types
- */
-export const AMOUNT = {
-  Fixed: "Fixed",
-  Percent: "Percent",
-  Multiplier: "Multiplier",
-} as const
-export type AmountValue = (typeof AMOUNT)[keyof typeof AMOUNT]
--- packages/contracts/src/components/resources.ts
+++
@@ -1,9 +0,0 @@
-/**
- * Resource types
- */
-export const RESOURCE = {
-  Coins: "Coins",
-  Lives: "Lives",
-  Both: "Both",
-} as const
-export type ResourceValue = (typeof RESOURCE)[keyof typeof RESOURCE]
