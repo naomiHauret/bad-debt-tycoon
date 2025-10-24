45067ef4bfef62ee3de7cdb35d1235986a533b06
naomiHauret <hauretnaomi@gmail.com>
2025-10-24 14:53:50 +0200

    chore(engine): export useful features from engine

---
+++ packages/engine/src/index.ts
@@ -0,0 +1,13 @@
+export {
+  isCombatCard,
+  isInstantCard,
+  isModifierCard,
+  prepareCardForRegistration,
+  validateCardTemplate,
+} from "./features/catalog/cards"
+export {
+  encodeObjectiveTarget,
+  generateAllObjectivesForType,
+  prepareObjectiveForRegistraton,
+  validateObjectiveTarget,
+} from "./features/catalog/objectives"
