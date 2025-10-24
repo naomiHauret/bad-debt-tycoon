47a76d4dc36d3cabe12bdc7a84f8b1e9321c190c
naomiHauret <hauretnaomi@gmail.com>
2025-10-24 16:25:57 +0200

    chore(parameters): lint & format

--- packages/engine/src/parameters/factory/factory-parameters.ts
+++ packages/engine/src/parameters/factory/factory-parameters.ts
@@ -1,7 +1,7 @@
-import HARDHAT from "./factory.hardhat.json"
-import ARBITRUM_SEPOLIA from "./factory.arbitrum-sepolia.json"
 import { PLATFORM_FEE } from "@/engine/data-structures/fees"
+import ARBITRUM_SEPOLIA from "./factory.arbitrum-sepolia.json"
+import HARDHAT from "./factory.hardhat.json"
 
 export const FACTORY_PLATFORM_FEE = PLATFORM_FEE
-export const FACTORY_PARAMETERS_HARDHAT = {...HARDHAT} as const 
-export const FACTORY_PARAMETERS_ARBITRUM_SEPOLIA = {...ARBITRUM_SEPOLIA} as const 
+export const FACTORY_PARAMETERS_HARDHAT = { ...HARDHAT } as const
+export const FACTORY_PARAMETERS_ARBITRUM_SEPOLIA = { ...ARBITRUM_SEPOLIA } as const
