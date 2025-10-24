7f8c67939cea317dcdeb3ee7bf3777d9ca1b115f
naomiHauret <hauretnaomi@gmail.com>
2025-10-24 14:13:21 +0200

    chore(whitelist): setup chain-specific modules for token whitelist deployments and setup

---
+++ packages/engine/ignition/modules/TokenWhitelist.hardhat.ts
@@ -0,0 +1,19 @@
+import { buildModule } from "@nomicfoundation/hardhat-ignition/modules"
+import { TOKEN_LIST } from "@/engine/assets/tokens/arbitrum-sepolia"
+
+// biome-ignore lint/style/noDefaultExport: -
+export default buildModule("TokenWhitelist", (m) => {
+  // Deploy & populate
+  const whitelist = m.contract("TournamentTokenWhitelist")
+  TOKEN_LIST.forEach((token) => {
+    m.call(whitelist, "addToken", [token.address], {
+      id: `whitelisted_token_${token.id}_${token.address}`,
+    })
+  })
+
+  return { whitelist }
+})
+
+// Sorry for code dedupe
+// Wanted deploy a specific token list per chain, but it's not available yet
+// https://github.com/NomicFoundation/hardhat/issues/7552
